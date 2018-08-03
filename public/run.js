f045aaf2ecaa6beb9ace93eb633fc58a55e55ed0
Doanh <congdoanh95@gmail.com>
2018-08-03 17:21:29 +0700

    doanh cty

--- public/run.js
+++ public/run.js
@@ -1,13 +1,22 @@
 //import Red from './components/Red';
-
 class Yellow extends React.Component {
-    getThongtin() {
-        alert(123456);
+    constructor(props) {;
+        super(props);
+        this.state = {
+            'strSearch': 'anh doanh',
+            'number': 10
+        };
+        this.addItem = this.addItem.bind(this);
     }
+    addItem() {
+       this.setState({number: this.state.number + 1});
+    }
+
     render() {
         return  <div>
             <h1 className="yellow">Đây là màu vàng</h1>
-            <button onClick={this.getThongtin} >Click</button>
+            <p>Số lượng: {this.state.number}</p>
+            <button onClick={this.addItem}>Tăng</button>
         </div>;
     }
 }
